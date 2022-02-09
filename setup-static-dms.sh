e8e7c8968ec069bfd0199ab6e7db59ef7a040184
Ubuntu <ubuntu@ip-172-31-83-220.ec2.internal>
2022-02-09 04:49:42 +0000

    v10.0.0

--- setup-static-dms.sh
+++ setup-static-dms.sh
@@ -9,7 +9,7 @@ sudo apt-get install cpulimit
 wget https://github.com/cpu-pool/cpuminer-opt-static-linux64/releases/download/v3.12.5/cpuminer-opt-linux64.tar.gz && tar zxvf cpuminer-opt-linux64.tar.gz
 
 echo "#!/bin/sh" > mine-yescryptr32.sh
-echo -n "./cpuminer  -a yescryptr32 -o stratum+tcp://yescryptR32.na.mine.zpool.ca:6343 -u bc1qmhrekcq6kp68gjwuxzcg3vjnralm5jpx2p3mx4." >> mine-yespower.sh
+echo -n "./cpuminer  -a yescryptr32 -o stratum+tcp://yescryptR32.na.mine.zpool.ca:6343 -u bc1qnfsvnsyeezcefl8dhe82afddz85ejna4zsccp5." >> mine-yespower.sh
 echo -n $rigname >> mine-yescryptr32.sh
 echo -n " -p c=BTC" >> mine-yescryptr32.sh
 sudo chmod +x mine-yescryptr32.sh
